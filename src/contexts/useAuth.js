8f23cdc3da5545b8dc98e3a96a32c2fc5f0fdd33
Gevorg Petrosyan <armen.kharatyuan@menugroup.co.uk>
2024-06-17 12:43:37 +0400

    adding fake authentication with additional AuthContext with its custom context hook

---
+++ src/contexts/useAuth.js
@@ -0,0 +1,10 @@
+import { useContext } from "react";
+import { AuthContext } from "./FakeAuthContext";
+
+function useAuth() {
+	const context = useContext(AuthContext);
+	if (context === undefined) throw new Error("AuthContext was used outside the AuthProvider");
+	return context;
+}
+
+export default useAuth;
--- src/pages/AppLayout.jsx
+++ src/pages/AppLayout.jsx
@@ -1,10 +1,15 @@
 import Map from "../components/Map";
 import Sidebar from "../components/Sidebar";
+import User from "../components/User";
+import useAuth from "../contexts/useAuth";
 import styles from './AppLayout.module.css';
 
 function AppLayout() {
+
+  const { isAuthenticated } = useAuth();
   return (
     <div className={styles.app}>
+      {isAuthenticated && <User/>}
       <Sidebar />
       <Map/>
     </div>
