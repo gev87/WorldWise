c911fe920b295a8a585a5d98f7e451870de6ee05
Gevorg Petrosyan <armen.kharatyuan@menugroup.co.uk>
2024-06-18 10:35:35 +0400

    adding protectedRoute to wrap all the components to handle the cases the user is logged in or not

--- src/pages/AppLayout.jsx
+++ src/pages/AppLayout.jsx
@@ -1,19 +1,16 @@
 import Map from "../components/Map";
 import Sidebar from "../components/Sidebar";
 import User from "../components/User";
-import useAuth from "../contexts/useAuth";
-import styles from './AppLayout.module.css';
+import styles from "./AppLayout.module.css";
 
 function AppLayout() {
-
-  const { isAuthenticated } = useAuth();
-  return (
-    <div className={styles.app}>
-      {isAuthenticated && <User/>}
-      <Sidebar />
-      <Map/>
-    </div>
-  );
+	return (
+		<div className={styles.app}>
+			<User />
+			<Sidebar />
+			<Map />
+		</div>
+	);
 }
 
 export default AppLayout;
